225024cd36c5bc5f525127e634f370b5f8b5e000
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-28 02:10:24 +0000

    chore(docker): change message error and docker folder name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,9 +7,9 @@ printmainstep "Déclenchement de la release des microservices"
 printstep "Vérification des paramètres d'entrée"
 init_env
 
-DOCKER_DIR=dockererr
+DOCKER_DIR=docker
 if [ ! -d $DOCKER_DIR ]; then
-    printerror "Impossible de trouver le dossier contenant les unit systemd docker $CONF_DIR dans le projet"
+    printerror "Impossible de trouver le dossier $DOCKER_DIR contenant les services docker dans le projet"
     exit 1
 else
     ls -l $DOCKER_DIR
