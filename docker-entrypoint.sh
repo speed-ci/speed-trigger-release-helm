9ac7587259bd8d2322aa6cfb47107d5a6de5345e
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-05-02 23:56:05 +0000

    feat(api): add job release api call

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,7 +12,30 @@ if [ ! -d $DOCKER_DIR ]; then
     printerror "Impossible de trouver le dossier $DOCKER_DIR contenant les services docker dans le projet"
     exit 1
 else
-    ls -l $DOCKER_DIR
+    SERVICE_LIST=$DOCKER_DIR/*.service
+    for SERVICE in $SERVICE_LIST
+    do
+        PROJECT_DEPLOY_NAME=$(basename "$SERVICE")
+        PROJECT_DEPLOY_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$SERVICE" | jq .[0].id`
+        
+        if [[ $PROJECT_DEPLOY_ID != "null" ]]; then
+        
+            printstep "Préparation du lancement du job release sur le projet $PROJECT_DEPLOY_NAME"
+            LAST_COMMIT=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/repository/commits?per_page=1&page=1" | jq .[0].id | tr -d '"'`
+            JOB_RELEASE_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/jobs" | jq --arg commit_id "$LAST_COMMIT_ID" '.[] | select(.commit.id == "\($commit_id)" and .status == "manual" and .ref == "master")'`
+        
+            if [[ -n $JOB_RELEASE_ID ]]; then
+                printinfo "Déclenchement de la release sur le projet $PROJECT_DEPLOY_NAME"
+                curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/jobs/$JOB_RELEASE_ID/play" | jq
+            else
+                printwarn "Pas de déclenchement de release possible, le projet $PROJECT_DEPLOY_NAME ne dispose pas de jbo release disponible pour le commit $LAST_COMMIT" 
+            fi
+        
+        else
+            printerror "Pas de déclenchement de release possible, le projet $PROJECT_DEPLOY_NAME n'existe pas"
+            exit 1
+        fi
+    done
 fi
 
 exit 1
