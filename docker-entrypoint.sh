ad043e999b1b6b96d64503b2d4e539b970df5925
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-06-02 15:22:57 +1100

    feat(print): display project name and project namespace

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,29 +17,29 @@ else
     SERVICE_LIST=$DOCKER_DIR/*$SERVICE_EXT
     for SERVICE in $SERVICE_LIST
     do
-        PROJECT_DEPLOY_NAME=$(basename "$SERVICE" $SERVICE_EXT)
-        if [[ $PROJECT_DEPLOY_NAME == "*" ]]; then
+        PROJECT_RELEASE_NAME=$(basename "$SERVICE" $SERVICE_EXT)
+        if [[ $PROJECT_RELEASE_NAME == "*" ]]; then
             printerror "Aucun service docker trouvé respectant le format $SERVICE_LIST"
             exit 1
         fi
-        PROJECT_DEPLOY_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_DEPLOY_NAME" | jq .[0].id`
+        PROJECT_DEPLOY_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_RELEASE_NAME" | jq .[0].id`
         
-        if [[ $PROJECT_DEPLOY_ID != "null" ]]; then
+        if [[ $PROJECT_RELEASE_ID != "null" ]]; then
         
-            printstep "Préparation du lancement du job release sur le projet $PROJECT_DEPLOY_NAME"
-            LAST_COMMIT_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/repository/commits?per_page=1&page=1" | jq .[0].id | tr -d '"'`
-            JOB_RELEASE_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/jobs" | jq --arg commit_id "$LAST_COMMIT_ID" '.[] | select(.commit.id == "\($commit_id)" and .name == "release" and .status == "manual" and .ref == "master")' | jq .id`
+            printstep "Préparation du lancement du job release sur le projet $PROJECT_NAMESPACE/$PROJECT_RELEASE_NAME"
+            LAST_COMMIT_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_RELEASE_ID/repository/commits?per_page=1&page=1" | jq .[0].id | tr -d '"'`
+            JOB_RELEASE_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_RELEASE_ID/jobs" | jq --arg commit_id "$LAST_COMMIT_ID" '.[] | select(.commit.id == "\($commit_id)" and .name == "release" and .status == "manual" and .ref == "master")' | jq .id`
             echo "JOB_RELEASE_ID : $JOB_RELEASE_ID"
         
-            if [[ -n $JOB_RELEASE_ID ]]; then
-                printinfo "Déclenchement de la release sur le projet $PROJECT_DEPLOY_NAME"
-                curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -XPOST "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/jobs/$JOB_RELEASE_ID/play" | jq .
+            if [[ $JOB_RELEASE_ID != "" ]]; then
+                printinfo "Déclenchement de la release sur le projet $PROJECT_NAMESPACE/$PROJECT_RELEASE_NAME"
+                curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -XPOST "$GITLAB_API_URL/projects/$PROJECT_RELEASE_ID/jobs/$JOB_RELEASE_ID/play" | jq .
             else
-                printwarn "Pas de déclenchement de release possible, le projet $PROJECT_DEPLOY_NAME ne dispose pas de job release disponible pour le commit $LAST_COMMIT" 
+                printwarn "Pas de déclenchement de release possible, le projet $PROJECT_NAMESPACE/$PROJECT_RELEASE_NAME ne dispose pas de job release disponible pour le commit $LAST_COMMIT" 
             fi
         
         else
-            printerror "Pas de déclenchement de release possible, le projet $PROJECT_DEPLOY_NAME n'existe pas"
+            printerror "Pas de déclenchement de release possible, le projet $PROJECT_NAMESPACE/$PROJECT_RELEASE_NAME n'existe pas"
             exit 1
         fi
     done
