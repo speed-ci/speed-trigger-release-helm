aced3267e71b54192c73e378d127fef7d0b724da
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-06-02 15:38:46 +1100

    fix(id): filter release project with namespace

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,7 +22,7 @@ else
             printerror "Aucun service docker trouvé respectant le format $SERVICE_LIST"
             exit 1
         fi
-        PROJECT_RELEASE_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_RELEASE_NAME" | jq .[0].id`
+        PROJECT_RELEASE_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_RELEASE_NAME" | jq --arg project_namespace "$PROJECT_NAMESPACE" '.[] | select(.namespace.name == "\($project_namespace)")' | jq .[0].id`
         
         if [[ $PROJECT_RELEASE_ID != "null" ]]; then
         
