375d74a0da93417ccd4ed6fd08c92d3b7e2a30d6
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-07-24 14:50:54 +1100

    fix(api): avoid restricted users endpoint by passing GITLAB_CI_USER_ID as a var env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -80,7 +80,6 @@ do
     if [[ $PROJECT_RELEASE_ID != "null" ]]; then
     
         printstep "Préparation du projet $PROJECT_NAMESPACE/$PROJECT_RELEASE_NAME"
-        GITLAB_CI_USER_ID=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/users?username=$GITLAB_CI_USER" | jq .[0].id`
         GITLAB_CI_USER_MEMBERSHIP=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_RELEASE_ID/members?query=$GITLAB_CI_USER" | jq .[0]`
         if [[ $GITLAB_CI_USER_MEMBERSHIP == "null" ]]; then 
             printinfo "Ajout du user $GITLAB_CI_USER manquant au projet $PROJECT_NAMESPACE/$PROJECT_RELEASE_NAME"
