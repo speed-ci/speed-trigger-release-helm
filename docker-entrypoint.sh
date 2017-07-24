b8bba3395456f8d5f7a6e57a81002f36c94d3b44
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-07-25 10:06:48 +1100

    fix(check): test empty string to check if project already treated

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -77,7 +77,7 @@ do
     echo "PROJECT_RELEASE_NAME: $PROJECT_RELEASE_NAME"
     echo "PROJECT_RELEASE_IDS[PROJECT_RELEASE_NAME] : ${PROJECT_RELEASE_IDS[$PROJECT_RELEASE_NAME]}"
     
-    if  [[ -z ${PROJECT_RELEASE_IDS[$PROJECT_RELEASE_NAME]} ]]; then
+    if  [[ ${PROJECT_RELEASE_IDS[$PROJECT_RELEASE_NAME]} == "" ]]; then
     
         echo "Step inside traitment"
         PROJECT_RELEASE_ID=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_RELEASE_NAME" | jq --arg project_namespace "$PROJECT_NAMESPACE" '.[] | select(.namespace.name == "\($project_namespace)") | .id'`
