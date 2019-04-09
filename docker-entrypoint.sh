638a7377a4b902125dbb3499f8a889bdc8824e2e
Mathieu Filotto <m.filotto@eramet-sln.nc>
2019-04-10 08:48:24 +1100

    feat(jobs): activate search systemd service file for jobs if name different from values file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -208,20 +208,15 @@ do
             CHANGELOG=$(printf "$CHANGELOG\n - Service **$PROJECT_NAMESPACE-$COMPLETE_ALIAS** : Projet Gitlab associé **$PROJECT_RELEASE_NAME [$PROJECT_RELEASE_VERSION]($GITLAB_URL/$PROJECT_NAMESPACE/$PROJECT_RELEASE_NAME/tags/$PROJECT_RELEASE_VERSION)**")
             if [ -d $DOCKER_DIR ]; then
                 SERVICE=$DOCKER_DIR/$PROJECT_NAMESPACE-$COMPLETE_ALIAS$SERVICE_EXT
-                SERVICE_URL_ENCODED=`echo $SERVICE | sed -e "s/\//%2F/g" | sed -e "s/\./%2E/g"`
-                SERVICE_FILE_FROM_RELEASE=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_ID/repository/files/$SERVICE_URL_ENCODED/raw?ref=release"`
-                echo "SERVICE_FILE_FROM_RELEASE: $SERVICE_FILE_FROM_RELEASE"
-                if [[ $ALIAS == "jobs" ]]; then
+                if [[ ! -f $SERVICE && $ALIAS == "jobs" ]]; then
                     STREAM_NAME=`yq r $HELM_VALUES $COMPLETE_ALIAS.streamName`
-                    echo "KS STREAM_NAME: $STREAM_NAME"
                     if [[ $STREAM_NAME == "null" ]]; then
                         STREAM_NAME=`yq r $HELM_VALUES $COMPLETE_ALIAS.task.name`
-                        echo "SAMZA STREAM_NAME: $STREAM_NAME"
                     fi
-                    grep --help
-                    SERVICE_FILE_NAME=`grep -rl $STREAM_NAME docker`
-                    echo "SERVICE_FILE_NAME: $SERVICE_FILE_NAME"
+                    SERVICE=`grep -rl $STREAM_NAME docker`
                 fi
+                SERVICE_URL_ENCODED=`echo $SERVICE | sed -e "s/\//%2F/g" | sed -e "s/\./%2E/g"`
+                SERVICE_FILE_FROM_RELEASE=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_ID/repository/files/$SERVICE_URL_ENCODED/raw?ref=release"`
                 VERSION_FOUND=`echo $SERVICE_FILE_FROM_RELEASE | grep $PROJECT_NAMESPACE/$PROJECT_RELEASE_NAME:$PROJECT_RELEASE_VERSION-part-of-$RELEASE_VERSION | wc -l`
                 if [[ $VERSION_FOUND == 0 ]]; then
                     printinfo "Injection de la version $PROJECT_NAMESPACE/$PROJECT_RELEASE_NAME:$PROJECT_RELEASE_VERSION-part-of-$RELEASE_VERSION dans le fichier de service systemd $SERVICE"
